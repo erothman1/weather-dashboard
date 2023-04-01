190a3324d026924b86c7ef0d1f91ab0c06c32332
Elva Rothman <erothman@tulane.edu>
2023-04-01 11:51:30 -0400

    created click event for submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,13 @@ var country
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
 
-fetch(queryURL)
+
+var userFormEl = document.getElementById("user-form")
+
+function formSubmitHandler(event) {
+    event.preventDefault()
+
+    console.log("hello")
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler)
